473f1dec28f2d150da603ebae0dccaa34e011587
22peasel <49483145+22peasel@users.noreply.github.com>
2019-06-12 12:34:13 -0400

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,8 @@
+
+def main():
+    kilometers = float(input("Enter a number [represents kilometers]: "))
+    m = 1.609 #m stands for miles
+    miles = round(kilometers / m,5)
+    print("This is the distance in miles:", miles)
+main()
+
